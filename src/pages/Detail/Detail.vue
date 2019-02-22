38cd6e4e8b4c06036112dd7039b3d4903f20ccb4
libowen <1064416560@qq.com>
2019-02-22 20:29:10 +0800

    :sparkles: feat:完成详情页的功能

--- src/pages/Detail/Detail.vue
+++ src/pages/Detail/Detail.vue
@@ -29,6 +29,13 @@
       <div class="tip">若发现实际工作内容与上述描述不符，请您提高警惕并第一时间向我们举报</div>
       <div class="color-block"></div>
     </div>
+    <div class="operationArea" >
+      <div class="btn stop" v-show="isShowStop" @click="stopRecruit()">停止招聘</div>
+      <div class="btn evaluate" v-show="isShowEvaluate" @click="toEvaluate()">前去评价</div>
+      <div class="btn start" v-show="isShowStart" @click="startIt()">开工</div>
+      <div class="btn rePublish" v-show="isShowRePublish" @click="rePublish()">重新发布</div>
+    </div>
+    <div class="color-block"></div>
   </div>
 </template>
 
@@ -51,8 +58,13 @@ export default {
         hireNum: '',
         nowNum: '',
         details: '',
-        isSignOrNot: ''
-      }
+        isSignOrNot: '',
+        status: null
+      },
+      isShowStop: false,
+      isShowEvaluate: false,
+      isShowStart: false,
+      isShowRePublish: false
     }
   },
   methods: {
@@ -65,6 +77,7 @@ export default {
           console.log(res)
           this.list = res.data.data
           this.list.jobType = this.getTypeName(this.list.jobType)
+          this.checkStatus()
         })
     },
     // 比较兼职类型码获取类型文字
@@ -76,6 +89,92 @@ export default {
           return typeList[x].name
         }
       }
+    },
+    // 检测状态码显示可执行的操作
+    checkStatus () {
+      console.log(this.list)
+      let num = this.list.status
+      // 在正在展示与满员情况下可以停止招聘
+      if (num === 1 || num === 3) {
+        this.isShowStop = true
+      } else if (num === 4) {
+        this.isShowStart = true
+        this.isShowRePublish = true
+      } else if (num === 8) {
+        this.isShowEvaluate = true
+      }
+    },
+    // 停止招聘
+    stopRecruit () {
+      this.$layer.closeAll()
+      this.$layer.confirm('您确定要停止吗？', () => {
+        this.$axios.post('/merchant/job/updateJobById.do', qs.stringify({
+          jobId: this.list.jobId,
+          action: 4
+        }))
+          .then(res => {
+            if (res.data.status === 2000) {
+              this.$layer.closeAll()
+              this.$layer.msg('删除成功')
+              // 1秒后回到我的兼职页面
+              setTimeout(() => {
+                this.$router.replace('/parttimejob/myparttimejob')
+              }, 1000)
+            } else {
+              this.$layer.closeAll()
+              this.$layer.msg(res.data.msg)
+            }
+          })
+      })
+    },
+    // 前去评价
+    toEvaluate () {
+    },
+    // 开工
+    startIt () {
+      this.$layer.closeAll()
+      this.$layer.confirm('您确定要开工吗？', () => {
+        this.$axios.post('/merchant/job/updateJobById.do', qs.stringify({
+          jobId: this.list.jobId,
+          action: 7
+        }))
+          .then(res => {
+            if (res.data.status === 2000) {
+              this.$layer.closeAll()
+              this.$layer.msg('成功开工')
+              // 1秒后回到我的兼职页面
+              setTimeout(() => {
+                this.$router.replace('/parttimejob/myparttimejob')
+              }, 1000)
+            } else {
+              this.$layer.closeAll()
+              this.$layer.msg(res.data.msg)
+            }
+          })
+      })
+    },
+    // 重新发布
+    rePublish () {
+      this.$layer.closeAll()
+      this.$layer.confirm('您确定要重新发布吗？', () => {
+        this.$axios.post('/merchant/job/updateJobById.do', qs.stringify({
+          jobId: this.list.jobId,
+          action: 1
+        }))
+          .then(res => {
+            if (res.data.status === 2000) {
+              this.$layer.closeAll()
+              this.$layer.msg('发布成功')
+              // 1秒后回到我的兼职页面
+              setTimeout(() => {
+                this.$router.replace('/parttimejob/myparttimejob')
+              }, 1000)
+            } else {
+              this.$layer.closeAll()
+              this.$layer.msg(res.data.msg)
+            }
+          })
+      })
     }
   },
   mounted () {
@@ -85,7 +184,8 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-@import '~@/assets/styles/varibles.styl'
+@import '~@/assets/styles/varibles'
+@import '~@/assets/styles/mixin'
   .item-title
     font-size .38rem
     padding .4rem .4rem
@@ -121,4 +221,26 @@ export default {
     font-weight bold
     text-indent .48rem
     color $color-info
+  .operationArea
+    width 100%
+    height 2rem
+    display flex
+    justify-content space-around
+    .btn
+      width 2rem
+      height 1rem
+      border-radius .5rem
+      color $color-text
+      font-size $font-size-medium-x
+      text-align center
+      line-height 1rem
+      margin-top .5rem
+    .stop
+      background $color-danger
+    .evaluate
+      background $color-info
+    .start
+      background $color-success
+    .rePublish
+      background $color-theme
 </style>
